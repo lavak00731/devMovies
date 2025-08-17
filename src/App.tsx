76d68221b90ba4d06b1dffb72b3422804811c7d0
Ezequiel <eolavaca@gmail.com>
2025-08-17 19:10:24 -0300

    announcing each navigation change using aria-live polite for sr users

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { RoutesElem } from './routes/RoutesElem'
 import './App.css'
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
 
 function App() {
-
+  const location = useLocation();
+  const [announcement, setAnnouncement] = useState('');
+  useEffect(() => {
+    const pageTitle = document.title; // Or extract from route data
+    setAnnouncement(`Navigated to ${pageTitle}`);
+  }, [location]); // Update announcement when location changes
 
   return (
     <>
+      <p aria-live="polite" className="sr-only">{announcement}</p>
       <RoutesElem/>
     </>
   )
