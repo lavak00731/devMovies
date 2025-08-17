b07f2aee5f5d32ae58824a79febc406f4a5cacd8
Ezequiel <eolavaca@gmail.com>
2025-08-17 20:11:07 -0300

    mend logo url

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router";
-import logo from "./../../public/logo.png";
 import { Nav } from "./Nav";
 export const Header = () => {
   return (
     <header>
-        <Link to="/"><img src={logo} alt="To Home Page" /></Link>
+        <Link to="/"><img src="/logo.png" alt="To Home Page" /></Link>
         <Nav />
     </header>
   )
