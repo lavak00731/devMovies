d6a743153aec8065176a2bc3b77adff0b530d67e
Ezequiel <eolavaca@gmail.com>
2025-08-21 14:21:08 -0300

    adjust on result page

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import type { MovieInterface } from "../types/MovieInterface"
 import  MovieCard from "./MovieCard";
 import { useSelector } from "react-redux";
@@ -5,16 +6,30 @@ import { useSelector } from "react-redux";
 
 export const Result = ({response, movie}:{response: string, movie?: MovieInterface}) => {
   const term = useSelector((state:any) => state.search.term);
+  const movies = useSelector((state:any) => state.search.movies);
+  const prevLength = useRef(movies.length);
+  const [isMoviesAdded, setisMoviesAdded] = useState(false)
+  useEffect(() => {
+    if (movies.length > prevLength.current) {
+    setisMoviesAdded(true)
+  }
+  prevLength.current = movies.length;
+  
+    return () => {
+      setisMoviesAdded(false)
+    }
+  }, [movies.length])
+  
   console.log(term)
   return (
     <div className="">
-        {term !== '' && (
+        {term !== ''&& isMoviesAdded && (
           <p aria-live="polite">
-            {response ? "One result for your search" : "Please try again, no results found"}
+            { response ? "One result for your search" : "Please try again, no results found"}
           </p>
         )}
 
-        {response ? <MovieCard movie={movie}/> : null }
+        {response && movie && isMoviesAdded  ? <MovieCard movie={movie}/> : null }
     </div>
     
   )
