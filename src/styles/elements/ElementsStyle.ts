ecd656a92df668a925b7de2ba01d169ced274723
Ezequiel <eolavaca@gmail.com>
2025-08-21 08:02:54 -0300

    adding focus visible styling to menu button

--- src/styles/elements/ElementsStyle.ts
+++ src/styles/elements/ElementsStyle.ts
@@ -4,7 +4,7 @@ const ElementStyle = {
     mainNav: "bg-zinc-50 absolute top-20 md:top-26 lg:top:32 right-[15%] w-full max-w-[250px] p-2 border-2 border-neutral-700 rounded shadow-[6px_17px_13px_-11px_#2E3532]",
     logo: "block max-w-24 focus-visible:outline-solid focus-visible:outline-2 focus-visible:outline-sky-800 cursor-pointer md:max-w-36",
     btn: "block rounded text-center w-full bg-sky-800 text-base px-2 py-1 text-zinc-50 border-2 border-sky-800 font-bold .libertinus-sans-regular hover:bg-zinc-50 hover:text-sky-800 focus-visible:bg-zinc-50 focus-visible:text-sky-800",
-    menuBtn: "bg-zinc-800 p-2 rounded text-green-400 text-base absolute top-[50%] translate-y-[-50%] right-[15%]",
+    menuBtn: "bg-zinc-800 p-2 rounded text-green-400 text-base absolute top-[50%] translate-y-[-50%] right-[15%] focus-visible:bg-green-400 focus-visible:text-zinc-800",
     menuGrid: "flex flex-col flex-wrap gap-4 items-end"
 }
 export default ElementStyle;
