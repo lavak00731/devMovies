ddad1a03d06fa48eb880b1513c8a9b6f49c9e151
Ezequiel <eolavaca@gmail.com>
2025-08-21 09:04:15 -0300

    skip link fixed

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,20 +1,30 @@
-import React, { type ReactNode } from 'react';
+import { type ReactNode } from 'react';
+import { useRef } from 'react';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
+
 
 interface MainLayoutProps {
   children: ReactNode;
 }
 
-const MainLayout: React.FC<MainLayoutProps> = ({ children }) => (
-  <>
-    <a className="focus:not-sr-only sr-only" href="#main-content">Skip to main content</a>
-    <Header />
-    <main id="#main-content" aria-labelledby="pageHeading">
-      {children}
-    </main>
-    <Footer />
-  </>
-);
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) =>{
+  const btnRef = useRef<HTMLElement>(null);
+  const handleClick = () => {
+    if (btnRef.current) btnRef.current.focus();
+  }
+ 
+
+  return  (
+    <>
+      <button className="focus:not-sr-only sr-only" onClick={handleClick}>Skip to main content</button>
+      <Header />
+      <main ref={btnRef}  tabIndex={-1} aria-labelledby="pageHeading">
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+}  
 
 export default MainLayout;
